abbfd5f70957dc64d7296527c10b5b28a2c8371a
Wenqiang Xie <wq.xie@outlook.com>
2020-09-14 16:44:13 +0800

    update exFacialCurve

--- exFacialCurve.py
+++ exFacialCurve.py
@@ -1,4 +1,8 @@
 
-def exFacialCurve(res:int=100, p:float=2, rp:list=list(range(30,80,10)), npt):
+from surfacePlot import surfacePlot
 
+
+def exFacialCurve(vertex:tuple, res:int=100, p:float=2, rp:list=list(range(30,80,10)), npt):
+
+    [qx, qy, qz] = surfacePlot(vertex[0], vertex[1], vertex[2], res)
     pass
