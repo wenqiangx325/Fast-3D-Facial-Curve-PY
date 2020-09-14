a98c8d497d738c05df005e02c5507df0187146c3
Wenqiang Xie <wq.xie@outlook.com>
2020-09-14 16:33:21 +0800

    update surfacePlot

--- surfacePlot.py
+++ surfacePlot.py
@@ -1,6 +1,7 @@
 import numpy
 from scipy.interpolate import griddata
 
+
 def surfacePlot(x3: numpy.ndarray, y3: numpy.ndarray, z3: numpy.ndarray, res):
 
     x3 = x3 - x3[z3 == numpy.max(z3)]
@@ -12,5 +13,11 @@ def surfacePlot(x3: numpy.ndarray, y3: numpy.ndarray, z3: numpy.ndarray, res):
         numpy.linspace(numpy.min(y3), numpy.max(y3), res)
     )
 
-    qz33 = griddata()
-    pass
+    qz33 = griddata(
+        numpy.array(list(zip(x3, y3))),
+        z3,
+        (qx33, qy33),
+        method='linear'
+    )
+    
+    return qx33, qy33, qz33
